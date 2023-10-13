9ba5fb276d4ef6fe32dd3e747e67154c80c68744
Joshua Bissell <joahgreat12@gmail.com>
2023-10-13 22:00:06 +0100

    Update INNUINSTALL.lua

--- INNU/INNUINSTALL.lua
+++ INNU/INNUINSTALL.lua
@@ -4,6 +4,7 @@ function Install()
     fileURLs = {
         "https://github.com/joshgreatuk/InnoCC/raw/main/INNU/INNUBEEP.lua" }
     for i, url in ipairs(fileURLs) do
+        fileName = fileURLs:
         shell.run("wget", url, "INNU/")
     end
 end
@@ -17,7 +18,8 @@ fs.makeDir("INNU")
 print("Installing INNUExtension. . .\n")
 
 shell.run("wget", "https://github.com/joshgreatuk/InnoCC/raw/main/INNU/INNUExtension.lua", "INNU/INNUExtension.lua")
-os.loadAPI("INNU/INNUExtension.lua")
+package.loadlib("INNUExtension","INNU/INNUExtension" )
+require("INNUExtension")
 
 print("Installing INNU. . .\n")
 Install()
