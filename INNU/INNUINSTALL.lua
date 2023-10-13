dcfa89d8a234b1710a2094b87f704706ce1aa029
Joshua Bissell <joahgreat12@gmail.com>
2023-10-13 21:19:49 +0100

    Update INNUINSTALL.lua

--- INNU/INNUINSTALL.lua
+++ INNU/INNUINSTALL.lua
@@ -3,17 +3,14 @@
 function Install()
     fileURLs = { 
         "https://github.com/joshgreatuk/InnoCC/raw/main/INNU/INNUBEEP.lua" }
-
-    for i=0, 0, fileURLs.len do
-        if not fs.isDir("INNU") then fs.makeDir("INNU") end
-        
-        for existing in fs.list("INNU/") do
-            fs.delete(existing)
-        end
-
-        for url in fileURLs do
-            shell.run("wget", url, "INNU/")
-        end
+    if not fs.isDir("INNU") then fs.makeDir("INNU") end
+    
+    for existing in fs.list("INNU/") do
+        fs.delete(existing)
+    end
+    
+    for url in fileURLs do
+        shell.run("wget", url, "INNU/")
     end
 end
 
